6e13bc6d14c518c5550f2d07a028ffc2fca03dce
Makieephie <Qchiamakaephraim@gmail.com>
2022-07-25 16:48:58 -0700

    Task 6

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -6,6 +6,8 @@
 class Rectangle:
     """Represents a 2D Polygon with 4 perpendicular sides.
     """
+    number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         """Initializes a Rectangle with a given width and height.
         Args:
@@ -14,6 +16,7 @@ class Rectangle:
         """
         self.width = width
         self.height = height
+        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
