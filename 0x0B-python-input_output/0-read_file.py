dd83b4228cf346a80287e34b9db3a528d868655f
Makieephie <Qchiamakaephraim@gmail.com>
2022-08-02 13:03:13 -0700

    Task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-"""
-This program reads a text file in format UTF-8
-"""
+"""defines a function that reads a text file (UTF8) and prints to stdout"""
+
 
 def read_file(filename=""):
-    """Open in a secure way the file, to be printed"""
+    """ -Reads and prints text file in UTF8 encoding.
+    -Makes use of the 'with' statement to simplify our
+    write process
+    """
     with open(filename, encoding='utf-8') as file:
         print(file.read(), end="")
     file.close()
