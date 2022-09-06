9df5d9ebb91af0841a4b67924b075299936f7d80
Makieephie <Qchiamakaephraim@gmail.com>
2022-09-06 08:04:37 -0700

    Task 1

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,3 +1,3 @@
 -- Creates a new database
--- Query to create the database hbtn_oc_o in MySQL server if it does not exist
-CREATE DATABASE IF NOT EXISTS hbtn_oc_
+-- Query to create the database hbtn_Oc_O in MySQL server if it does not exist
+CREATE DATABASE IF NOT EXISTS hbtn_Oc_O;
